63dcc283760842c16afbe867127c219af6552490
RafaelSouzaCostaa <rafael.souza@estudante.ifgoiano.edu.br>
2022-01-16 00:33:07 -0300

    Todos os Algoritmos Finalizado
    Todos os algoritmos finalizados - verificar novamente atentamente e reparar algum possível bug.

--- Main.java
+++ Main.java
@@ -1,21 +1,67 @@
 public class Main {
     public static void main(String[] args) {
-        //Global.matrizGlobal10x5(2, 1);
-        //Global.setValorMatriz10x5(2, 1, 500);
-        //Global.matrizGlobal10x5(2, 1);
-
+    
+        System.out.println( "---------------- Algoritomo NRU ------------- \n\n Start");
+        
         Global.criarMatrizPreenchida100x5();
         Global.preencherMatriz10x5ComSorteoMatriz100x5();
 
-        for(int i = 0; i < 1000; i++) {
-            if(i == 900){
+        for(int i = 0; i < 100; i++) {
+            if(i == 99){
+                Global.startSimulador(AlgoritimosConstantes.NRU, true);
+                break;
+            }
+
+            Global.startSimulador(AlgoritimosConstantes.NRU, false);
+        }
+
+
+
+
+        System.out.println( "---------------- Algoritomo FIFO ------------- \n\n Start");
+        
+        Global.criarMatrizPreenchida100x5();
+        Global.preencherMatriz10x5ComSorteoMatriz100x5();
+
+        for(int i = 0; i < 100; i++) {
+            if(i == 99){
                 Global.startSimulador(AlgoritimosConstantes.FIFO, true);
+                break;
             }
 
             Global.startSimulador(AlgoritimosConstantes.FIFO, false);
         }
 
-        //Global.imprimirMatriz(Global.getMatriz100x5(), "DISCO 100x5");
 
+
+        System.out.println( "---------------- Algoritomo FIFO-SC ------------- \n\n Start");
+        
+        Global.criarMatrizPreenchida100x5();
+        Global.preencherMatriz10x5ComSorteoMatriz100x5();
+
+        for(int i = 0; i < 100; i++) {
+            if(i == 99){
+                Global.startSimulador(AlgoritimosConstantes.FIFO_SC, true);
+                break;
+            }
+
+            Global.startSimulador(AlgoritimosConstantes.FIFO_SC, false);
+        }
+
+
+
+        System.out.println( "---------------- Algoritomo RELOGIO ------------- \n\n Start");
+        
+        Global.criarMatrizPreenchida100x5();
+        Global.preencherMatriz10x5ComSorteoMatriz100x5();
+
+        for(int i = 0; i < 100; i++) {
+            if(i == 99){
+                Global.startSimulador(AlgoritimosConstantes.RELOGIO, true);
+                break;
+            }
+
+            Global.startSimulador(AlgoritimosConstantes.RELOGIO, false);
+        }
     }
 }
